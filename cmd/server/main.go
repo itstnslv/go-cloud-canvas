9896fb073aeb5ef5bd0768a9f199eaf45ffd960a
Stanislav Ovsyankin <stanislav.ovsyankin@levelblue.com>
2025-04-11 23:28:26 +0500

    Create a start function

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,9 +3,106 @@
 package main
 
 import (
+	"canvas/server"
+	"context"
 	"fmt"
+	"go.uber.org/zap"
+	"golang.org/x/sync/errgroup"
+	"os"
+	"os/signal"
+	"strconv"
+	"syscall"
 )
 
+// release is set through the linker at build time, generally from a git sha.
+// Used for logging and error reporting.
+var release string
+
 func main() {
-	fmt.Println("🤓")
+	os.Exit(start())
+}
+
+func start() int {
+	logEnv := getStringOrDefault("LOG_ENV", "development")
+	log, err := createLogger(logEnv)
+	if err != nil {
+		fmt.Println("error setting up the logger:", err)
+		return 1
+	}
+
+	log = log.With(zap.String("release", release))
+
+	defer func() {
+		// If we cannot sync, there's probably something wrong with outputting logs,
+		// so we probably cannot write using fmt.Println either. So just ignore the error.
+		_ = log.Sync()
+	}()
+
+	host := getStringOrDefault("HOST", "localhost")
+	port := getIntOrDefault("PORT", 8080)
+
+	s := server.New(server.Options{
+		Host: host,
+		Log:  log,
+		Port: port,
+	})
+
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGTERM, syscall.SIGINFO)
+	defer stop()
+	eg, ctx := errgroup.WithContext(ctx)
+
+	eg.Go(func() error {
+		if err := s.Start(); err != nil {
+			log.Info("Error starting server", zap.Error(err))
+			return err
+		}
+		return nil
+	})
+
+	<-ctx.Done()
+
+	eg.Go(func() error {
+		if err := s.Stop(); err != nil {
+			log.Info("error stopping server", zap.Error(err))
+			return err
+		}
+		return nil
+	})
+
+	if err := eg.Wait(); err != nil {
+		return 1
+	}
+
+	return 0
+}
+
+func getIntOrDefault(name string, defaultV int) int {
+	v, ok := os.LookupEnv(name)
+	if !ok {
+		return defaultV
+	}
+	vAsInt, err := strconv.Atoi(v)
+	if err != nil {
+		return defaultV
+	}
+	return vAsInt
+}
+
+func createLogger(env string) (*zap.Logger, error) {
+	switch env {
+	case "production":
+		return zap.NewProduction()
+	case "development":
+		return zap.NewDevelopment()
+	default:
+		return zap.NewNop(), nil
+	}
+}
+
+func getStringOrDefault(name string, defaultV string) string {
+	v, ok := os.LookupEnv(name)
+	if !ok {
+		return defaultV
+	}
+	return v
 }
